7b106c51fd981fbd667ad649b14651ab3a611c39
晨晨的小迷妹 <46657072+luckiana@users.noreply.github.com>
2021-05-17 12:19:21 +0200

    update

--- docs/.vuepress/util/getQuery.js
+++
@@ -1,3 +0,0 @@
-export default function getQuery() {
-    return window.location.search;
-}
--- docs/.vuepress/util/settings.js
+++
@@ -1,6 +0,0 @@
-import settings from '../settings/index.js'
-
-export function getSettings() {
-    // return a copy of settings to prevent modification
-    return JSON.parse(JSON.stringify(settings))
-}
